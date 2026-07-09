1b7ebb9f2c5261f58ed2c076bf490b94b91e34a5
Tazrian Chowdhury <tazrianchowdhury2020@gmail.com>
2026-07-09 20:39:30 +0600

    Register engagement models in admin

--- backend/engagement/admin.py
+++ backend/engagement/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Notification, UserEngagement, Badge, UserBadge, Mission, UserMissionProgress
 
-# Register your models here.
+admin.site.register(Notification)
+admin.site.register(UserEngagement)
+admin.site.register(Badge)
+admin.site.register(UserBadge)
+admin.site.register(Mission)
+admin.site.register(UserMissionProgress)
