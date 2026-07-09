3dcd7689c02dd1e07676d09d9e52691ccba4031b
Tazrian Chowdhury <tazrianchowdhury2020@gmail.com>
2026-07-09 20:38:56 +0600

    Register social models in admin

--- backend/social/admin.py
+++ backend/social/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Interest, UserInterest, Post, Like, Comment
 
-# Register your models here.
+admin.site.register(Interest)
+admin.site.register(UserInterest)
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
