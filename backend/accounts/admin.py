d512f7437d541a02891fad75ebd933b2daef33f6
Tazrian Chowdhury <tazrianchowdhury2020@gmail.com>
2026-07-09 20:38:45 +0600

    Register accounts models in admin

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import University, User, Profile
 
-# Register your models here.
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'name')
+    search_fields = ('domain', 'name')
+
+admin.site.register(User)
+admin.site.register(Profile)
