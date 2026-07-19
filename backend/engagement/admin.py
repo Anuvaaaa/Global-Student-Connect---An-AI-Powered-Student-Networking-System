ee5f2b641d60100660399ab1d627b53206e157fe
Fahima Afrin Anuva <165905293+Anuvaaaa@users.noreply.github.com>
2026-07-19 17:17:42 +0600

    Delete backend/engagement/admin.py

--- backend/engagement/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Notification, UserEngagement, Badge, UserBadge, Mission, UserMissionProgress
-
-admin.site.register(Notification)
-admin.site.register(UserEngagement)
-admin.site.register(Badge)
-admin.site.register(UserBadge)
-admin.site.register(Mission)
-admin.site.register(UserMissionProgress)
